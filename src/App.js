f0ef3f6789ecd271288b627963e85479ca34f69a
viji9898 <vijitha.wijesuriya@gmail.com>
2022-04-09 18:29:07 +0100

    add-axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [helloWorld, setHelloWorld] = useState("");
+
+  const getHelloWorld = () => {
+    axios
+      .get(".netlify/functions/getHelloWorld", {
+        baseURL: "/",
+      })
+      .then(function (response) {
+        setHelloWorld(response.data.data);
+      });
+  };
+  useEffect(() => {
+    getHelloWorld();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>From Faunadb:</p>
+        <p>
+          <code>"{helloWorld.title}"</code>.
         </p>
         <a
           className="App-link"
