44b84bab617fe80907fccfbb2f36eb475eac8900
viji9898 <vijitha.wijesuriya@gmail.com>
2022-09-06 22:51:18 +0300

    update-hero-layout

--- src/components/landingPage/hero.js
+++ src/components/landingPage/hero.js
@@ -11,19 +11,42 @@ export const Hero = ({ helloWorld }) => {
         maxWidth: "1200px",
       }}
     >
-      <Col xs={24} sm={24} md={12} style={{ textAlign: "center" }}>
-        <Row justify="center">
+      <Col xs={24} sm={24} md={12}>
+        <Row>
           <Col span={24}>
-            <p>Boiler Plate App</p>
+            <div
+              style={{
+                fontFamily: "Open Sans, sans-serif",
+                fontSize: "16px",
+                opacity: "70%",
+                marginLeft: "50px",
+              }}
+            >
+              <h1> - Boiler Plate App - </h1>
+            </div>
           </Col>
         </Row>
-        <Row justify="center">
-          <Col span={6}>
-            <p>Boiler Plate App</p>
+        <Row
+          gutter={[18, 18]}
+          style={{
+            fontFamily: "Open Sans, sans-serif",
+            fontSize: "16px",
+            opacity: "70%",
+            marginLeft: "50px",
+          }}
+        >
+          <Col xs={24} sm={24} md={12}>
+            <div style={{ fontWeight: "600" }}>
+              <h2>Instructions</h2>
+            </div>
+            <p>Checkout the developer docs on how to use the app</p>
             <Button>Developer Docs</Button>
           </Col>
-          <Col span={6}>
-            <p>Boiler Plate App</p>
+          <Col xs={24} sm={24} md={12}>
+            <div style={{ fontWeight: "600" }}>
+              <h2>Developer</h2>
+            </div>
+            <p>Checkout the repo for instruction on how to use the app</p>
             <Button
               href="https://github.com/viji9898/app-boiler-plate"
               target="_blank"
